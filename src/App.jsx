3a2f772d23227b05d731c4b712eee0601abe1801
Kelvin Gatungo <kelvin.gatungo@gmail.com>
2025-05-27 00:30:02 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,47 @@
 import React, { useState } from "react";
-import './App.css';
-import AboutUs from './AboutUs';
-import { Provider } from "react-redux";
-import store from "../redux/store";
-import ProductList from "./components/ProductList";
-import Cart from "./components/Cart";
+import ProductList from "./ProductList";
+import "./App.css";
+import AboutUs from "./AboutUs";
 
 function App() {
   const [showProductList, setShowProductList] = useState(false);
 
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
+  };
+
+  const handleHomeClick = () => {
+    setShowProductList(false);
+  };
+
   return (
-    <Provider store={store}>
-      <div className="app-container">
-        <button onClick={() => setShowProductList(!showProductList)}>
-          {showProductList ? "Hide Products" : "Show Products"}
-        </button>
-        {showProductList && <ProductList />}
-        <Cart />
-        <AboutUs />
+    <div className="app-container">
+      <div className={`landing-page ${showProductList ? "fade-out" : ""}`}>
+        <div className="background-image"></div>
+        <div className="content">
+          <div className="landing_content">
+            <h1>Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
+
+            <button
+              className="get-started-button"
+              onClick={handleGetStartedClick}
+            >
+              Get Started
+            </button>
+          </div>
+          <div className="aboutus_container">
+            <AboutUs />
+          </div>
+        </div>
       </div>
-    </Provider>
+      <div
+        className={`product-list-container ${showProductList ? "visible" : ""}`}
+      >
+        <ProductList onHomeClick={handleHomeClick} />
+      </div>
+    </div>
   );
 }
 
